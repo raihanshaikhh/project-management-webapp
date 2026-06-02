0b59e4dfe3d196385b9c2e46b2b4adb40e1e3fdf
raihanshaikhh <raihanshaikh91201@gmail.com>
2026-06-02 22:43:35 +0530

    add env config for deployment

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,10 +1,9 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
   withCredentials: true
 })
-
 API.interceptors.request.use((req) => {
 
   const token = localStorage.getItem("token")

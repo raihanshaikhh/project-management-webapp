60498e5a99fd33ef11ceac250831fca80633b9d4
raihanshaikhh <raihanshaikh91201@gmail.com>
2026-06-03 22:44:56 +0530

    socket issue fixed

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000"); // match your backend port
+const socket = io(import.meta.env.VITE_API_URL?.replace("/api/v1", "") || "http://localhost:8000");// match your backend port
 
 export default socket;
